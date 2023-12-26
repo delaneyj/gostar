7eb996fc8da3690fa24745f8077cf8368406f350
Delaney Gillilan <delaneygillilan@gmail.com>
2023-12-26 10:12:55 -0800

    missing span

--- cfg/spec_html.go
+++ cfg/spec_html.go
@@ -1962,6 +1962,11 @@ var HTML = &pb.Namespace{
 		},
 
 		{
+			Tag:         "span",
+			Description: `The <span> HTML element is a generic inline container for phrasing content, which does not inherently represent anything. It can be used to group elements for styling purposes (using the class or id attributes), or because they share attribute values, such as lang. It should be used only when no other semantic element is appropriate. <span> is very much like a <div> element, but <div> is a block-level element whereas a <span> is an inline-level element.`,
+		},
+
+		{
 			Tag:         "strike",
 			Description: "The HTML <strike> element (or HTML Strikethrough Element) places a strikethrough (horizontal line) over text.",
 		},
